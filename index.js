9d329616a45fbe8df792a7c241434cf370c54675
pratiksharm <sharma.pratik2016@gmail.com>
2019-06-29 23:20:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/',function(req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: '+ msg);
+    });
+})
+io.on('connection', function(socket) {
+    socket.broadcast.emit('hi');
+});
+http.listen(3000, function() {
+    console.log('listening on port :3000');
+})
